b9c23beea3f48a1ccd26c3857b94d871cf391458
Carlton Joseph <carlton.joseph@gmail.com>
2017-11-08 20:03:37 -0800

    add skeleton for ui

---
+++ components/UdaciSlider.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function UdaciSlider (props) {
+  return (<View>
+  <Text>UdaciSlider:Value={props.value}</Text>
+  </View>);
+}
---
+++ components/UdaciSteppers.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function UdaciSteppers (props) {
+  return (<View>
+    <Text>UdaciSteppers: Value={props.value}</Text>
+  </View>);
+}
