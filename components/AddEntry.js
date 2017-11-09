51ed3d2738f57dd008d36276434c0dfca53d1a4d
Carlton Joseph <carlton.joseph@gmail.com>
2017-11-08 20:23:17 -0800

    add tochables

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
-import { getMetricMetaInfo } from '../utils/helpers';
+import { View, Text, TouchableOpacity } from 'react-native';
+import { getMetricMetaInfo, timeToString } from '../utils/helpers';
 import UdaciSlider from './UdaciSlider';
 import UdaciSteppers from './UdaciSteppers';
 import DateHeader from './DateHeader';
 
+function SubmitBtn ({onPress}) {
+  return (<TouchableOpacity onPress={onPress}>
+    <Text>Submit</Text>
+  </TouchableOpacity>);
+}
+
 export default class AddEntry extends Component {
-  state = {
-    run: 0,
-    bike: 0,
-    swim: 0,
-    sleep: 0,
-    eat: 0,
-  }
+  state = { run: 0, bike: 0, swim: 0, sleep: 0, eat: 0, }
   increment = metric => {
     const { max, step } = getMetricMetaInfo(metric);
     this.setState(state => {
@@ -29,6 +29,15 @@ export default class AddEntry extends Component {
   }
   slide = (metric, value) => {
     this.setState(state => ({...state, [metric]: value}));
+  }
+  submit = () => {
+    const key = timeToString();
+    const entry = this.state;
+    // update redux
+    this.setState(() => ({ run: 0, bike: 0, swim: 0, sleep: 0, eat: 0, }));
+    // nav to home
+    // save to db
+    // clear local notification
   }
   render = () => {
     const metaInfo = getMetricMetaInfo();
@@ -53,6 +62,7 @@ export default class AddEntry extends Component {
               />}
         </View>);
       })}
+      <SubmitBtn onPress={this.submit} />
     </View>);
   }
 }
