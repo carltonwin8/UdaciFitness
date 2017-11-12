84b5a7acf84462fca21adbdacbb659297a126c6f
Carlton Joseph <carlton.joseph@gmail.com>
2017-11-12 15:17:02 -0800

    add stepper styles

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -14,8 +14,11 @@ import { getDailyReminderValue } from '../utils/helpers';
 import { white, purple } from '../utils/colors';
 
 function SubmitBtn ({onPress}) {
-  return (<TouchableOpacity onPress={onPress}>
-    <Text>Submit</Text>
+  return (
+  <TouchableOpacity
+    onPress={onPress}
+    style={Platform.OS === 'ios' ? styles.iosSubmitBtn : styles.androidSubmitBtn}>
+    <Text style={styles.submitBtnText}>Submit</Text>
   </TouchableOpacity>);
 }
 
@@ -56,22 +59,22 @@ class AddEntry extends Component {
   }
   render = () => {
     if (this.props.alreadyLogged) {
-      return (<View>
+      return (<View style={styles.center}>
         <Ionicons
-          name='ios-happy-outline'
+          name={Platform.OS === 'ios' ? 'ios-happy-outline' : 'md-happy'}
           size={100}
         />
         <Text>You already logged your information for today 2.</Text>
-        <TextButton onPress={this.reset}>Reset</TextButton>
+        <TextButton onPress={this.reset} style={{padding: 10}}>Reset</TextButton>
       </View>);
     } else {
       const metaInfo = getMetricMetaInfo();
-      return (<View>
+      return (<View style={styles.container}>
         <DateHeader date={(new Date()).toLocaleDateString()} />
         {Object.keys(metaInfo).map(key => {
           const { getIcon, type, ...rest } = metaInfo[key];
           const value = this.state[key];
-          return (<View key={key}>
+          return (<View key={key} style={styles.row}>
             {getIcon()}
             {type === "steppers"
               ? <UdaciSteppers
@@ -97,31 +100,45 @@ class AddEntry extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    padding: 20,
     backgroundColor: white,
   },
-  // iosSubmitBtn: {
-  //   backgroundColor: purple,
-  //   padding: 10,
-  //   borderRadius: 7,
-  //   height: 45,
-  //   marginLeft: 40,
-  //   marginRight: 40,
-  // },
-  // androidSubmitBtn: {
-  //   backgroundColor: purple,
-  //   padding: 10,
-  //   borderRadius: 2,
-  //   height: 45,
-  //   marginLeft: 30,
-  //   marginRight: 30,
-  //   justifyContent: 'center',
-  //   alignItems: 'center'
-  // },
-  // submitBtnText: {
-  //   color: white,
-  //   fontSize: 22,
-  //   textAlign: 'center',
-  // },
+  row: {
+    flexDirection: 'row',
+    flex: 1,
+    alignItems: 'center',
+  },
+  iosSubmitBtn: {
+    backgroundColor: purple,
+    padding: 10,
+    borderRadius: 7,
+    height: 45,
+    marginLeft: 40,
+    marginRight: 40,
+  },
+  androidSubmitBtn: {
+    backgroundColor: purple,
+    padding: 10,
+    borderRadius: 2,
+    height: 45,
+    marginLeft: 30,
+    marginRight: 30,
+    alignSelf: 'flex-end',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  submitBtnText: {
+    color: white,
+    fontSize: 22,
+    textAlign: 'center',
+  },
+  center: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 30,
+    marginRight: 30,
+  }
 });
 
 function mapStateToProps (state) {
--- components/UdaciSteppers.js
+++ components/UdaciSteppers.js
@@ -1,20 +1,69 @@
 import React from 'react';
-import { View, Text , TouchableOpacity } from 'react-native';
+import { View, Text , TouchableOpacity, Platform, StyleSheet } from 'react-native';
 import { FontAwesome, Entypo } from '@expo/vector-icons';
+import { white, gray, purple } from '../utils/colors';
 
 export default function UdaciSteppers ({value, onInc, onDec, unit, max, step}) {
-  return (<View>
-    <View>
-      <TouchableOpacity onPress={onDec} >
-        <FontAwesome name='minus' size={30} color={'black'} />
-      </TouchableOpacity>
-      <TouchableOpacity onPress={onInc} >
-        <FontAwesome name='plus' size={30} color={'black'} />
-      </TouchableOpacity>
-    </View>
-    <View>
-      <Text>{value}</Text>
-      <Text>{unit}</Text>
+  return (<View style={[styles.row, { justifyContent: 'space-between'}]}>
+    {Platform.OS === 'ios'
+      ?
+        <View style={{flexDirection: 'row'}}>
+          <TouchableOpacity
+            onPress={onDec}
+            style={[styles.iosBtn, {borderTopRightRadius: 0, borderBottomRightRadius: 0}]}>
+            <Entypo name='minus' size={30} color={'purple'} />
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={onInc}
+            style={[styles.iosBtn, {borderTopLeftRadius: 0, borderBottomLeftRadius: 0}]}>
+            <Entypo name='plus' size={30} color={'purple'} />
+          </TouchableOpacity>
+        </View>
+      :
+        <View style={{flexDirection: 'row'}}>
+          <TouchableOpacity
+            onPress={onDec}
+            style={[styles.androidBtn]}>
+            <FontAwesome name='minus' size={30} color={'white'} />
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={onInc}
+            style={[styles.androidBtn]}>
+            <FontAwesome name='plus' size={30} color={'white'} />
+          </TouchableOpacity>
+        </View>
+    }
+    <View style={[styles.metricCounter]}>
+      <Text style={{fontSize: 28, textAlign: 'center'}}>{value}</Text>
+      <Text style={{fontSize: 18, color: gray}}>{unit}</Text>
     </View>
   </View>);
 }
+
+const styles = StyleSheet.create({
+  row: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  iosBtn: {
+    backgroundColor: white,
+    borderColor: purple,
+    borderWidth: 1,
+    borderRadius: 3,
+    padding: 5,
+    paddingLeft: 25,
+    paddingRight: 25,
+  },
+  androidBtn: {
+    margin: 5,
+    backgroundColor: purple,
+    padding: 10,
+    borderRadius: 2,
+  },
+  metricCounter: {
+    width: 85,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
